9715d9dcbecbee20d8c9650afa64e803d61c8db1
photocracker <dmitry.a.kotikov@gmail.com>
2023-02-23 13:38:32 +0100

    updated PWA functionality

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const cacheName = 'my-app-cache';
+
+const filesToCache = [
+  '/',
+  '/index.html',
+  '/favicon.ico',
+  '/manifest.json',
+  '/src/components/Ducts.vue',
+  '/src/App.vue',
+  '/css/style.css'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
--- src/components/Ducts_backup.vue
+++ src/components/Ducts_backup.vue
@@ -1,21 +1,24 @@
-<template><!-- #TODO - to create offline DUCTS with id 555prefix. then when go online check if they are exist -->
+<template>
+  <!-- #TODO - to create offline DUCTS with id 555prefix. then when go online check if they are exist -->
   <div class="hello">
 
     <h1>{{ msg }}</h1>
-    <span :class="{ 'text-green': onlineStatus === 'Online work', 'text-red': onlineStatus === 'Offline work' }">{{
-      onlineStatus }}</span>
-    <button @click="switchOnOffline()" style="margin-left:10px"> {{ buttonOnlineText }} </button>
-
+    <span :class="{ 'text-green': onlineStatus === 'Online work', 'text-red': onlineStatus === 'Offline work' }">{{ onlineStatus }}</span>
+    <button @click="switchOnOffline()" style="margin-left:10px" >  {{ buttonOnlineText }} </button>
+    
     <h2>Get or update the ducts</h2>
     {{ ducts }} <br><br>
 
     <!-- <select @change="getDuctPointData" v-model="selectedDuctId" > -->
     <select @change="getDuctPoints" v-model="selectedDuctId">
       <option value="">Select Duct</option>
+      <!-- <option v-for="planId in planIds" :key="planId.empno">{{  planId.NUMID }}</option> -->
+
       <option v-for="ductItem in ducts" :key="ductItem.duct_id">{{ ductItem.duct_id }}</option>
+
     </select>
 
-    <div v-if="ducts.length || apexData.items" style="text-align: center; margin-top: 20px;">
+    <div v-if="apexData.items && apexData.items.length" style="text-align: center; margin-top: 20px;">
 
 
       <form method="post" style="width:300px; margin: 0 auto; text-align: left" @submit.prevent="updateDucts">
@@ -30,26 +33,26 @@
 
     </div>
     <p v-else>Please choose from above</p>
-
+    
     <h2>Or add new duct</h2>
 
     <div v-if="ducts" style="text-align: center; margin-top: 20px;">
 
-      <form method="post" style="width:300px; margin: 0 auto; text-align: left" @submit.prevent="addDucts">
+<form method="post" style="width:300px; margin: 0 auto; text-align: left" @submit.prevent="addDucts">
 
-        <label for="duct_id">Duct id </label>
-        <input disabled id="duct_id" type="text" name="duct_id" v-model="ductIdNew" /><br><br>
+  <label for="duct_id">Duct id </label>
+  <input  disabled  id="duct_id" type="text" name="duct_id" v-model="ductIdNew"  /><br><br>
+  
+  <label for="company_name">Company name </label>
+  <input  id="company_name" type="text" name="company_name" v-model="companyNameNew" /><br><br>
+  <label for="duct_lat">Latitude </label>
+  <input id="duct_lat" type="text" name="duct_lat" v-model="ductLatNew" /><br><br>
+  <label for="duct_long">Longitude </label>
+  <input id="duct_long" type="text" name="duct_long" v-model="ductLongNew" /><br>
+  <button style="align-items: center; margin-top:10px" type="submit">Add Duct</button>
+</form>
 
-        <label for="company_name">Company name </label>
-        <input id="company_name" type="text" name="company_name" v-model="companyNameNew" /><br><br>
-        <label for="duct_lat">Latitude </label>
-        <input id="duct_lat" type="text" name="duct_lat" v-model="ductLatNew" /><br><br>
-        <label for="duct_long">Longitude </label>
-        <input id="duct_long" type="text" name="duct_long" v-model="ductLongNew" /><br>
-        <button style="align-items: center; margin-top:10px" type="submit">Add Duct</button>
-      </form>
-
-    </div>
+</div>
 
 
   </div>
@@ -86,26 +89,9 @@ export default {
   },
 
   methods: {
-
-    
-    
     getDuctPoints() {
-      if (this.onlineStatus === "Offline work") 
-     {
-    
-      if (this.selectedDuctId) {
-    this.ductId = this.selectedDuctId;
-    this.companyName = this.ducts[this.selectedDuctId-1].company_name;
-    this.ductLat = this.ducts[this.selectedDuctId-1].duct_lat;
-    this.ductLong = this.ducts[this.selectedDuctId-1].duct_long;
-    }
-      
-
-    }
-    else
-      {
-      if (this.selectedDuctId) {
-          axios.get(`
+        if (this.selectedDuctId) {
+        axios.get(`
         https://g0268f6dc90ba0e-devdb.adb.eu-frankfurt-1.oraclecloudapps.com/ords/apex_dmitrii/ducts/one?DUCT_ID=${this.selectedDuctId}`)
           .then(response => {
             this.apexData = response.data;
@@ -122,10 +108,6 @@ export default {
       } else {
         this.apexData = ""
       }
-
-    }
-  
-
     },
 
     addDucts() {
@@ -167,33 +149,33 @@ export default {
         });
     },
 
-    switchOnOffline() {
+    switchOnOffline(){
       if (this.onlineStatus == "Offline work") {
         this.fetchData()
-
+        
       }
       else
-        this.onlineStatus = "Offline work"
-
+      this.onlineStatus = "Offline work"
+      
     },
 
     fetchData() {
       axios.get('https://g0268f6dc90ba0e-devdb.adb.eu-frankfurt-1.oraclecloudapps.com/ords/apex_dmitrii/ducts/info')
-        .then(response => {
-          this.ducts = response.data.items; // assuming the response data is an object with an "items" property that contains the array of employees
-          this.ductIdNew = (this.ducts[this.ducts.length - 1].duct_id + 1)
-          this.companyNameNew = '',
-            this.ductLatNew = '',
-            this.ductLongNew = '',
-            this.ductIdNew = (this.ducts[this.ducts.length - 1].duct_id + 1)
-          this.onlineStatus = "Online work"
-        })
-        .catch(error => {
-          console.error(error);
-          // #TODO: tut nado poimat oshibku not internet
-          this.onlineStatus = "Offline work"
-        });
-
+      .then(response => {
+        this.ducts = response.data.items; // assuming the response data is an object with an "items" property that contains the array of employees
+        this.ductIdNew = (this.ducts[this.ducts.length-1].duct_id +1)
+        this.companyNameNew = '',
+      this.ductLatNew = '',
+      this.ductLongNew = '',
+      this.ductIdNew = (this.ducts[this.ducts.length-1].duct_id +1)
+      this.onlineStatus = "Online work"
+      })
+      .catch(error => {
+        console.error(error);
+        // #TODO: tut nado poimat oshibku not internet
+        this.onlineStatus = "Offline work" 
+      });
+   
     }
 
   },
@@ -203,12 +185,12 @@ export default {
     // const confirmLoad = confirm("Do you want to try to load information from the server?");
     // if (confirmLoad) {
     // eslint-disable-next-line
-    if (1 == 1) {
+    if (1==1) {
       this.onlineStatus = "Online work"
       axios.get('https://g0268f6dc90ba0e-devdb.adb.eu-frankfurt-1.oraclecloudapps.com/ords/apex_dmitrii/ducts/info')
         .then(response => {
           this.ducts = response.data.items; // assuming the response data is an object with an "items" property that contains the array of employees
-          this.ductIdNew = (this.ducts[this.ducts.length - 1].duct_id + 1)
+          this.ductIdNew = (this.ducts[this.ducts.length-1].duct_id +1)
         })
         .catch(error => {
           console.error(error);
@@ -226,30 +208,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-.text-green {
-  color: green;
-}
-
-.text-red {
-  color: red;
-}
-</style>
+  h3 {
+    margin: 40px 0 0;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
+  .text-green {
+    color: green;
+  }
+  
+  .text-red {
+    color: red;
+  }
+  </style>
   
